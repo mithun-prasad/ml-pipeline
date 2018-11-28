ae5465dd1ad067a7ac0f4e1712815f65c9c86a39
Mithun Prasad <mithunprasad@Mithuns-MacBook-Pro.local>
2018-11-28 18:12:17 +0000

    train with output

--- code/training/train.py
+++ code/training/train.py
@@ -1,44 +1,66 @@
-# Copyright (c) Microsoft. All rights reserved.
-# Licensed under the MIT license.
-
+import pickle
+from azureml.core import Workspace
+from azureml.core.run import Run
+import os
 from sklearn.datasets import load_diabetes
 from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
-from azureml.core.run import Run
 from sklearn.externals import joblib
-import os
 import numpy as np
+import json
+import subprocess
+from typing import Tuple, List
 
-os.makedirs('./outputs', exist_ok=True)
+# run_history_name = 'devops-ai'
+#os.makedirs('./outputs', exist_ok=True)
+# #ws.get_details()
+# Start recording results to AML
+#run = Run.start_logging(workspace = ws, history_name = run_history_name)
+run = Run.get_submitted_run()
 
-X, y = load_diabetes(return_X_y=True)
 
-run = Run.get_context()
+X, y = load_diabetes(return_X_y = True)
+columns = ['age', 'gender', 'bmi', 'bp', 's1', 's2', 's3', 's4', 's5', 's6']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+data = {
+    "train":{"X": X_train, "y": y_train},        
+    "test":{"X": X_test, "y": y_test}
+}
 
-X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                    test_size=0.2,
-                                                    random_state=0)
-data = {"train": {"X": X_train, "y": y_train},
-        "test": {"X": X_test, "y": y_test}}
+print('Running train.py')
 
-# list of numbers from 0.0 to 1.0 with a 0.05 interval
+# Randomly pic alpha
 alphas = np.arange(0.0, 1.0, 0.05)
+alpha=alphas[np.random.choice(alphas.shape[0], 1, replace=False)][0]
+print(alpha)
+run.log('alpha', alpha)
+reg = Ridge(alpha = alpha)
+reg.fit(data['train']['X'], data['train']['y'])
+preds = reg.predict(data['test']['X'])
+run.log('mse', mean_squared_error(preds, data['test']['y']))
 
-for alpha in alphas:
-    # Use Ridge algorithm to create a regression model
-    reg = Ridge(alpha=alpha)
-    reg.fit(data["train"]["X"], data["train"]["y"])
 
-    preds = reg.predict(data["test"]["X"])
-    mse = mean_squared_error(preds, data["test"]["y"])
-    run.log('alpha', alpha)
-    run.log('mse', mse)
+# Save model as part of the run history
+model_name = "sklearn_regression_model.pkl"
+# model_name = "."
 
-    model_file_name = 'ridge_{0:.2f}.pkl'.format(alpha)
-    # save model in the outputs folder so it automatically get uploaded
-    with open(model_file_name, "wb") as file:
-        joblib.dump(value=reg, filename=os.path.join('./outputs/',
-                                                    model_file_name))
+with open(model_name, "wb") as file:
+    joblib.dump(value = reg, filename = model_name)
 
-    print('alpha is {0:.2f}, and mse is {1:0.2f}'.format(alpha, mse))
+# upload the model file explicitly into artifacts 
+run.upload_file(name = './outputs/'+ model_name, path_or_stream = model_name)
+print('Uploaded the model {} to experiment {}'.format(model_name, run.experiment.name))
+dirpath = os.getcwd()
+print(dirpath)
+
+
+# register the model
+# run.log_model(file_name = model_name)
+# print('Registered the model {} to run history {}'.format(model_name, run.history.name))
+
+
+
+print('Following files are uploaded ')
+print(run.get_file_names())
+run.complete()
